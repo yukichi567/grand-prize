e66b241fa654990f2c184d61edf9adcd85d11c9f
Otsu01 <kudo.nonoka02@gmail.com>
2023-04-27 03:27:52 +0900

    [updata]敵の基底クラスのダメージ関数

--- Assets/Kudou/EnemyBase.cs
+++ Assets/Kudou/EnemyBase.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class EnemyBase : MonoBehaviour
 {
     [SerializeField, Header("HP最大値")]protected int _maxHP;
+    protected int _nowHP;
     [SerializeField, Header("攻撃力")]protected int _attackPower;
-    
+
+    private void Start()
+    {
+        _nowHP = _maxHP;
+    }
     /// <summary>EnemyのHP最大値を増やす関数</summary>
     /// <param name="upPoint">増やす値</param>
     public void MaxHPUP(int upPoint)
@@ -26,6 +31,15 @@ public class EnemyBase : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             //PlayerのHPー攻撃力
+        }
+    }
+
+    private void  Damage(int damage)
+    {
+        _nowHP -= damage;
+        if(_nowHP < 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
