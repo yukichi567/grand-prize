c8ab28350513d227d5caec3f967c851348f8b511
ninomiyahayato <a7pseej9@icloud.com>
2023-04-28 22:10:20 +0900

    bossの動き修正

--- Assets/Ninomiya/BossMove.cs
+++ Assets/Ninomiya/BossMove.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BossMove : MonoBehaviour
@@ -7,8 +6,8 @@ public class BossMove : MonoBehaviour
     Rigidbody2D _rb;
     [SerializeField] float _limitTime; //次の攻撃までの時間
     float _nowTime;　//次の攻撃までの時間
-    [SerializeField]float _moveSpeed; //bossの移動速度
-    [SerializeField]bool _arrived; //攻撃の際、一回だけ情報を取るためのbool
+    [SerializeField] float _moveSpeed; //bossの移動速度
+    [SerializeField] bool _arrived; //攻撃の際、一回だけ情報を取るためのbool
     [SerializeField] float _stopDistance; //playerに近づく距離
     Vector2 _playerPosTmp; //playerの位置取得
     Vector2 _bosspos; //bossの位置取得
@@ -17,6 +16,9 @@ public class BossMove : MonoBehaviour
     [SerializeField] bool _jumpBool = false;
     [SerializeField] Transform[] _enemyTransform; //敵を償還する場所
     [SerializeField] GameObject[] _enemys; //敵を入れる
+
+    [SerializeField] bool _attackBool = false;
+    int numRandom;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,40 +30,32 @@ public class BossMove : MonoBehaviour
     void Update()
     {
         BossQuaternion();
-        _nowTime += Time.deltaTime;
-        if (_nowTime > _limitTime)
+        if (_attackBool == false)
         {
-            int numRandam = Random.Range(0, 3);
-            //System.Random tmpRandam = new System.Random();
-            //tmpRandam.Next(3);
-            switch(numRandam)
-            {
-                case 0:
-                    {
-                        Attack();
-                        _nowTime = 0;
-                        break;
-                    }
-                case 1:
-                    {
-                        Attack1();
-                        _nowTime = 0;
-                        break;
-                    }
-                case 2:
-                    {
-                        Attack2();
-                        _nowTime = 0;
-                        break;
-                    }
-            }
+            _arrived = true;
+            numRandom = UnityEngine.Random.Range(0, 3);
+            Debug.Log(numRandom);
+            StartCoroutine("AttackTime");
         }
+        if (numRandom == 0)
+        {
+            Attack();
+        }
+        else if (numRandom == 1)
+        {
+            Attack1();
+        }
+        else if (numRandom == 2)
+        {
+            Attack2();
+        }
+
     }
     void BossQuaternion()
     {
         Vector2 playerPos = GameObject.Find("Player").transform.position;
         Vector2 bossPos = this.transform.position;
-        if(playerPos.x < bossPos.x) //Bossの向き変更
+        if (playerPos.x < bossPos.x) //Bossの向き変更
         {
             this.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
         }
@@ -72,9 +66,9 @@ public class BossMove : MonoBehaviour
     }
     void Attack()
     {
-        if(_arrived)
+        if (_arrived == true)
         {
-           _bosspos  = this.transform.position;
+            _bosspos = this.transform.position;
             Vector2 playerPos = GameObject.Find("Player").transform.position;
             _playerPosTmp = playerPos;
             _arrived = false;
@@ -82,8 +76,8 @@ public class BossMove : MonoBehaviour
         }
         Vector2 dir = (_playerPosTmp - _bosspos).normalized;
         _rb.velocity = dir * _moveSpeed;
-        _distance = Vector2.Distance(this.transform.position,GameObject.Find("Player").transform.position);
-        Debug.Log(_distance);
+        _distance = Vector2.Distance(this.transform.position, GameObject.Find("Player").transform.position);
+        //Debug.Log(_distance);
         if (_distance < _stopDistance)
         {
             _rb.velocity = Vector2.zero;
@@ -92,7 +86,7 @@ public class BossMove : MonoBehaviour
     }
     void Attack1()
     {
-        if (_arrived)
+        if (_arrived == true)
         {
             _bosspos = this.transform.position;
             Vector2 playerPos = GameObject.Find("Player").transform.position;
@@ -100,7 +94,7 @@ public class BossMove : MonoBehaviour
             _arrived = false;
             Debug.Log("呼ばれた");
         }
-        if(_jumpBool == false)
+        if (_jumpBool == false)
         {
             this.transform.position = Vector3.SmoothDamp(transform.position, new Vector3(_playerPosTmp.x / 2,
             _playerPosTmp.y + 10, 0f), ref _velocity, 0.5f);
@@ -109,16 +103,23 @@ public class BossMove : MonoBehaviour
     }
     void Attack2()
     {
-        for(int i = 0; i < _enemyTransform.Length; i++)
+        for (int i = 0; i < _enemyTransform.Length; i++)
         {
-            int enemyInstanceCount = Random.Range(0,_enemys.Length);
+            int enemyInstanceCount = UnityEngine.Random.Range(0, _enemys.Length);
             Instantiate(_enemys[enemyInstanceCount], _enemyTransform[enemyInstanceCount]);
         }
     }
     IEnumerator JumpAttack()
     {
-        Debug.Log("呼ばれましたよコルーチン");
+        //Debug.Log("呼ばれましたよコルーチン");
         yield return new WaitForSeconds(1.5f);
         _jumpBool = true;
+    }
+    IEnumerator AttackTime()
+    {
+        _attackBool = true;
+        yield return new WaitForSeconds(3f);
+        Debug.Log("呼ばれましたよコルーチン");
+        _attackBool = false;
     }
 }
