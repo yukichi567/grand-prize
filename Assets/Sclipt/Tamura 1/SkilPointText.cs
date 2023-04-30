d04500c053674fce046797a6ba36f8aae1026fbb
CHARA\meich <mei.charaa@gmail.com>
2023-04-30 13:04:05 +0900

    [update]強化時にスキルポイントを消費するようにした。Tamuraフォルダ内にプレハブ化した強化画面が入ってる

---
+++ Assets/Sclipt/Tamura 1/SkilPointText.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkilPointText : MonoBehaviour
+{
+    [SerializeField] private Text _skilPoint = default;
+    
+    public void SetSkilPointText(int point)
+    {
+        _skilPoint.text = $"スキルポイント:{point}";
+    }
+
+}
