e44a76fdcfda658ca3451f820876d8e632971475
UmenoKoudai <106291399+UmenoKoudai@users.noreply.github.com>
2023-06-18 23:08:36 +0900

    WGL化できるように修正中

--- Assets/Script/System/GrowText.cs
+++ Assets/Script/System/GrowText.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class GrowText : MonoBehaviour
 {
     [SerializeField] private Text _growInf = default;
+    [SerializeField] private Text _growInf2 = default;
     [SerializeField] private Text _costInf = default;
 
     /// <summary>今のステータスと、強化後のステータスをテキストに表示する</summary>
@@ -11,14 +12,15 @@ public class GrowText : MonoBehaviour
     /// <param name="nextState"></param>
     public void SetGrowInfText(float nowState, float nextState)
     {
-        _growInf.text = $"{nowState} → {nextState}";
+        _growInf.text = $"{nowState}";
+        _growInf2.text = $"{nextState}";
     }
 
     /// <summary>強化に使うコストをテキストに表示する</summary>
     /// <param name="cost"></param>
     public void SetCostInfText(int cost)
     {
-        _costInf.text = $"必要ポイント:{cost}";
+        _costInf.text = $"UsePoint:{cost}";
     }
 
     /// <summary>最大まで強化したときのテキストを表示する</summary>
@@ -26,6 +28,6 @@ public class GrowText : MonoBehaviour
     public void SetCompleteText(float state)
     {
         _growInf.text = $"{state}";
-        _costInf.text = $"最大まで強化済み";
+        _costInf.text = $"MaxStrengthen";
     }
 }
